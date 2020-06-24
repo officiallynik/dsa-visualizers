e8a70d7a76ddb7f95ca547dfbeb152f286d4430b
officiallynik <nikhilownstheworld@gmail.com>
2020-06-24 20:34:11 +0530

    sorting-visualizers beta-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 import SortingVisualizers from './containers/sorting-visualizers/mainApp'
-import TreeVisualizers from './containers/tree-visualizers/mainApp'
 
 import { Switch, Route } from 'react-router-dom'
 
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <Switch>
         <Route path="/sorting-visualizers" component={SortingVisualizers} />
-        <Route path="/tree-visualizers" component={TreeVisualizers} />
       </Switch>
     </div>
   );
